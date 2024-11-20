1bf6d8e0170b4631a17c7ce94bdfacd477857fbc
Yoongu Kwon <dbsrn1110@gmail.com>
2024-11-20 15:17:48 +0900

    2-2

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
@@ -8,19 +9,19 @@ const NewsPage = () => {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        fetch(
-            'https://content.guardianapis.com/search?api-key=cb5c8f1d-41e3-4481-b13e-7b075cf3e537&show-fields=thumbnail,headline,byline,bodyText'
-        )
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setNews(data.response.results);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        const fetchNews = async () => {
+            try {
+                const res = await axios.get(
+                    'https://content.guardianapis.com/search?api-key=cb5c8f1d-41e3-4481-b13e-7b075cf3e537&show-fields=thumbnail,headline,byline,bodyText'
+                );
+                console.log(res.data);
+                setNews(res.data.response.results);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchNews();
     }, []);
 
     return (
@@ -29,10 +30,18 @@ const NewsPage = () => {
             <ul className='divide-y px-5'>
                 {news.map((item, index) => (
                     <li key={item.id}>
-                        <Link href={'/'} className='py-2 block'>
-                            <Image src={item.fields.thumbnail} alt={item.webTitle} width={300} height={300} />
-                            <strong>{item.webTitle}</strong>
-                            <p className='text-gray-500 text-sm'>{item.webPublicationDate}</p>
+                        <Link href={item.webUrl} className='py-2 flex gap-4'>
+                            <Image
+                                src={item.fields.thumbnail}
+                                alt={item.webTitle}
+                                width={200}
+                                height={200}
+                                className='object-cover rounded w-1/3'
+                            />
+                            <div className='flex-1 flex flex-col justify-between'>
+                                <strong>{item.webTitle}</strong>
+                                <p className='text-gray-500 text-sm'>{item.webPublicationDate}</p>
+                            </div>
                         </Link>
                     </li>
                 ))}
